9191ec5021c8936c49b2fced3db7c891d073fff5
hamzhrdn <uhamzahr04@gmail.com>
2024-06-13 01:13:20 +0700

    Update

--- app/src/main/java/com/example/story/home/HomeFragment.kt
+++ app/src/main/java/com/example/story/home/HomeFragment.kt
@@ -10,6 +10,7 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.FragmentNavigator
+import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.transition.TransitionInflater
@@ -41,11 +42,29 @@ class HomeFragment : Fragment() {
             val description = story.description
             val photoUrl = story.photoUrl
 
+            val extras = FragmentNavigatorExtras(
+                imageView to imageView.transitionName,
+                nameView to nameView.transitionName,
+                descView to descView.transitionName
+            )
+
             val action = HomeFragmentDirections.actionHomeFragmentToDetailStoryFragment(
                 id, name, description, photoUrl
             )
             Log.d("HomeFragment", "Navigating to DetailStoryFragment with id: $id")
-            findNavController().navigate(action)
+//            findNavController().navigate(action)
+
+            findNavController()
+                .navigate(action,
+                    FragmentNavigator.Extras.Builder()
+                        .addSharedElements(
+                            mapOf(
+                                imageView to imageView.transitionName,
+                                nameView to nameView.transitionName,
+                                descView to descView.transitionName,
+                            )
+                        ).build()
+                )
         }
 
         val recyclerView = binding.rvHome
